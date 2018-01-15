336ed47d993a8c589219ebcdaa5f85e2a8ce554a
Paul <paulc2017@att.net>
2018-01-15 07:41:38 -0500

    added click on a blog entry's title on the main page and go to a blog's individual entry page

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(255))
+    body = db.Column(db.Text)
     removed = db.Column(db.Boolean)
 
     def __init__(self, title, body, removed):
@@ -20,32 +20,62 @@ class Blog(db.Model):
         self.body = body
         self.removed = removed
 
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return redirect("/Blog") 
 
-    if request.method == 'POST':
-        add_body = request.form['body']
-        new_body = Blog(add_body)
-        db.session.add(new_body)
-        db.session.commit()
+@app.route('/Blog', methods=['POST', 'GET'])
+def blog():
 
     post = Blog.query.all()
      
-    return render_template('add_new_post.html',title="Blogs R Us!", 
+    return render_template('blog.html',title="Blogs R Us!", 
         post=post)
 
 
-@app.route('/remove-post', methods=['POST'])
-def remove_post():
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
 
-    post_id = int(request.form['post-id'])
+    if request.method == 'POST':
+        add_body = request.form['body']
+        add_title = request.form['title']
+        add_removed = False
+        new_post = Blog(add_title,add_body, add_removed)
+        db.session.add(new_post)
+        db.session.commit()
+
+    else: 
+        return render_template('add_new_post.html',title="Blogs R Us!")
+
+    post = Blog.query.all()
+    
+    
+    
+    return render_template("blog.html",title="Blogs R Us!", post=post)
+    # return render_template('add_new_post.html',title="Blogs R Us!", 
+    #  post=post)
+
+
+
+@app.route('/show_post', methods=['POST', 'GET'])
+def post():
+    post_title = request.args.get("post_title")
+    post_body = request.args.get("post_body")
+    #post = Blog.query.get(post_id)
+    #title = post.title
+    #body = post.body"""
+   
+    return render_template('show_post.html', post_title= post_title, post_body=post_body)
+    
+
+@app.route('/remove_post', methods=['POST'])
+def remove_post(): 
+
+    post_id = request.form['post_id']
     post = Blog.query.get(post_id)
-    post.removed = True
-    db.session.add(post.removed)
-    db.session.commit()
+    
 
-    return redirect('/')
+    return render_template('/remove_post.html', post_id = post_id)
 
 
 if __name__ == '__main__':
